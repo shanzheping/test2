4a693713649a941fe415eb1ae16110fe05a72c27
AlphaHinex <AlphaHinex@gmail.com>
2016-08-04 19:59:19 +0800

    Check request after marshall

--- src/main/java/com/proper/enterprise/isj/webservices/utils/SignAdapter.java
+++ src/main/java/com/proper/enterprise/isj/webservices/utils/SignAdapter.java
@@ -3,11 +3,16 @@ package com.proper.enterprise.isj.webservices.utils;
 import com.proper.enterprise.isj.webservices.model.ReqModel;
 import com.proper.enterprise.platform.core.utils.ConfCenter;
 import com.proper.enterprise.platform.core.utils.MD5Util;
+import com.proper.enterprise.platform.core.utils.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.text.MessageFormat;
 
 public class SignAdapter extends XmlAdapter<String, ReqModel> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SignAdapter.class);
 
     @Override
     public ReqModel unmarshal(String v) throws Exception {
@@ -18,9 +23,13 @@ public class SignAdapter extends XmlAdapter<String, ReqModel> {
     public String marshal(ReqModel v) throws Exception {
         String sign = MessageFormat.format(ConfCenter.get("isj.template.sign.req"),
                 v.getFunCode(),
-                v.getReqEncrypted(),
+                new ReqEncryptedAdapter().marshal(v.getReqEncrypted()),
                 v.getUserId(),
                 ConfCenter.get("isj.key"));
+
+        LOGGER.debug("Sign before MD5: {}",
+                StringUtil.abbreviate(sign,
+                        Integer.parseInt(ConfCenter.get("isj.abbreviate.maxWidth"))));
         return "<![CDATA[" + MD5Util.md5Hex(sign) + "]]>";
     }
 
--- src/test/groovy/com/proper/enterprise/isj/webservices/utils/ComposeRequestTest.groovy
+++ src/test/groovy/com/proper/enterprise/isj/webservices/utils/ComposeRequestTest.groovy
@@ -1,5 +1,9 @@
 package com.proper.enterprise.isj.webservices.utils
+
 import com.proper.enterprise.isj.webservices.model.ReqModel
+import com.proper.enterprise.platform.core.utils.CipherUtil
+import com.proper.enterprise.platform.core.utils.ConfCenter
+import com.proper.enterprise.platform.core.utils.MD5Util
 import com.proper.enterprise.platform.test.AbstractTest
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,13 +18,38 @@ class ComposeRequestTest extends AbstractTest {
 
     @Test
     public void marshallReq() {
+        def funCode = '1111'
+        def userId = 'DEFAULT_USER'
+        def req = [HOST_ID: '123', IP: '456']
+
         def writer = new StringWriter()
         def m = new ReqModel()
-        m.setFunCode('1111')
-        m.setReqEncrypted([HOST_ID: '123', IP: '456'])
+        m.setFunCode(funCode)
+        m.setUserId(userId)
+        m.setReqEncrypted(req)
         marshaller.marshal(m, new StreamResult(writer))
-        println writer.toString().replace('&lt;', '<').replace('&gt;', '>');
 
+        def xml = new XmlParser().parseText(writer.toString())
+        assert xml.FUN_CODE.text() == "<![CDATA[$funCode]]>"
+        assert xml.USER_ID.text() == "<![CDATA[$userId]]>"
+        assert xml.SIGN_TYPE.text() == "<![CDATA[MD5]]>"
+
+        def reqEnc = '<REQ>'
+        req.each { key, value ->
+            reqEnc += "<$key><![CDATA[$value]]></$key>"
+        }
+        reqEnc += '</REQ>'
+        CipherUtil aes = CipherUtil.getInstance(
+                ConfCenter.get("isj.algorithm"),
+                ConfCenter.get("isj.mode"),
+                ConfCenter.get("isj.padding"),
+                ConfCenter.get("isj.key"),
+                Integer.parseInt(ConfCenter.get("isj.keySize")));
+        reqEnc = aes.encrypt(reqEnc);
+        assert xml.REQ_ENCRYPTED.text() == reqEnc
+
+        def sign = "FUN_CODE=$funCode&REQ_ENCRYPTED=$reqEnc&USER_ID=$userId&KEY=${ConfCenter.get('isj.key')}"
+        assert xml.SIGN.text() == "<![CDATA[${MD5Util.md5Hex(sign)}]]>"
     }
 
 }
